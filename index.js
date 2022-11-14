9020def24ea06f456b2ffa236d1bfb7d3ead163b
jleesys949 <jlee.post24@gmail.com>
2022-11-13 16:16:19 -0800

    Create default route

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (request, response) => {
+    response.send(`<h1>Welcome to the Phonebook!</h1>`);
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}\nLol, godspeed`)
+})
